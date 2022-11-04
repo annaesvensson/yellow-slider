041a1a1a76774245c035a6755c30fdf4fe5c657c
markseu <mark2011@mayberg.se>
2022-11-04 09:44:51 +0100

    Updated API, new way of checking variables

--- README-de.md
+++ README-de.md
@@ -1,6 +1,6 @@
 <p align="right"><a href="README-de.md">Deutsch</a> &nbsp; <a href="README.md">English</a> &nbsp; <a href="README-sv.md">Svenska</a></p>
 
-# Slider 0.8.17
+# Slider 0.8.18
 
 Bildergalerie mit Schieber.
 
--- slider.php
+++ slider.php
@@ -2,7 +2,7 @@
 // Slider extension, https://github.com/annaesvensson/yellow-slider
 
 class YellowSlider {
-    const VERSION = "0.8.17";
+    const VERSION = "0.8.18";
     public $yellow;         // access to API
     
     // Handle initialisation
@@ -18,11 +18,11 @@ class YellowSlider {
         $output = null;
         if ($name=="slider" && ($type=="block" || $type=="inline")) {
             list($pattern, $sorting, $style, $size, $autoplay) = $this->yellow->toolbox->getTextArguments($text);
-            if (empty($sorting)) $sorting = $this->yellow->system->get("sliderSorting");
-            if (empty($style)) $style = $this->yellow->system->get("sliderStyle");
-            if (empty($size)) $size = "100%";
-            if (empty($autoplay)) $autoplay = $this->yellow->system->get("sliderAutoplay");
-            if (empty($pattern)) {
+            if (is_string_empty($sorting)) $sorting = $this->yellow->system->get("sliderSorting");
+            if (is_string_empty($style)) $style = $this->yellow->system->get("sliderStyle");
+            if (is_string_empty($size)) $size = "100%";
+            if (is_string_empty($autoplay)) $autoplay = $this->yellow->system->get("sliderAutoplay");
+            if (is_string_empty($pattern)) {
                 $pattern = "unknown";
                 $files = $this->yellow->media->clean();
             } else {
@@ -32,7 +32,7 @@ class YellowSlider {
                 elseif ($sorting=="size") $files->sort("size", false);
             }
             if ($this->yellow->extension->isExisting("image")) {
-                if (count($files)) {
+                if (!is_array_empty($files)) {
                     $page->setLastModified($files->getModified());
                     $output = "<div class=\"splide\" data-arrows=\"false\" data-rewind=\"true\" data-clickable=\"true\" data-type=\"".htmlspecialchars($style)."\"";
                     if ($autoplay!=0) $output .= " data-autoplay=\"true\" data-interval=\"".htmlspecialchars($autoplay)."\"";
@@ -41,7 +41,7 @@ class YellowSlider {
                     foreach ($files as $file) {
                         list($src, $width, $height) = $this->yellow->extension->get("image")->getImageInformation($file->fileName, $size, $size);
                         $caption = $this->yellow->language->isText($file->fileName) ? $this->yellow->language->getText($file->fileName) : "";
-                        $alt = empty($caption) ? basename($file->getLocation(true)) : $caption;
+                        $alt = is_string_empty($caption) ? basename($file->getLocation(true)) : $caption;
                         $output .= "<div class=\"splide__slide\"><img src=\"".htmlspecialchars($src)."\"";
                         if ($width && $height) $output .= " width=\"".htmlspecialchars($width)."\" height=\"".htmlspecialchars($height)."\"";
                         $output .= " alt=\"".htmlspecialchars($alt)."\" title=\"".htmlspecialchars($alt)."\"";
