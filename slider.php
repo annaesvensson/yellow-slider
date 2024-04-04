102e821b493f60e2d2afdbcd0744491a6a8f2324
markseu <mark2011@mayberg.se>
2024-04-05 00:38:43 +0200

    Updated extension

--- slider.php
+++ slider.php
@@ -2,7 +2,7 @@
 // Slider extension, https://github.com/annaesvensson/yellow-slider
 
 class YellowSlider {
-    const VERSION = "0.8.19";
+    const VERSION = "0.9.1";
     public $yellow;         // access to API
     
     // Handle initialisation
